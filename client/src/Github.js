e7cf1e05ed509b4ff92f4588062fa2ee0eafc0d1
iamabhishekbharti <imabhishekbharti12@gmail.com>
2019-01-19 15:17:02 +0530

    client interface modified

--- client/src/Github.js
+++ client/src/Github.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-import {Row, Card, CardHeader,CardBody,CardTitle, Col, CardText} from 'reactstrap'
+import {Row, Card, CardSubtitle,CardBody,CardTitle, Col, CardImg} from 'reactstrap'
 import axios from 'axios'
+var cardStyle = {
+  padding: '20px'
+}
 
 class Github extends Component {
   state = {
@@ -16,16 +19,18 @@ class Github extends Component {
   render() {
     return (
       <>
+        <div>
+          <h1>Github 100 recent Users list</h1>
+        </div>
         <Row>  
           {this.state.users.map((user, index) => {
           return(
-            <Col md="4">
-              <Card body outline color="danger">
-                <CardHeader>
-                  <CardTitle tag="p" key={index}>{user.id}</CardTitle>
-                </CardHeader>
+            <Col md="4" style={cardStyle}>
+              <Card>
+              <CardImg top src={user.avatar_url} alt="..." />
                 <CardBody>
-                  <CardText>Username: {user.login}</CardText>
+                 <CardTitle tag="p" key={index}>Username: <b>{user.login}</b></CardTitle>
+                 <CardSubtitle>Id: {user.id}</CardSubtitle>
                 </CardBody>
               </Card>
             </Col>
---
+++ readMe.md
@@ -0,0 +1,11 @@
+Project of Guest Touch
+
+#To run this project 
+
+Tools used
+===============
+FrontEnd: React
+BackEnd: NodeJS
+-------------------
+```npm run dev```
+This command run both client and server concurrently
